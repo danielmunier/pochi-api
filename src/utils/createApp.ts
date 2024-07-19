b666c3bd3ed8a2af5a32f2af2629ca03bb3267e8
Daniel <idanielmunier@gmail.com>
2024-07-19 14:46:50 -0300

    fix domain session

--- src/utils/createApp.ts
+++ src/utils/createApp.ts
@@ -32,7 +32,7 @@ export function createApp(): Express {
             //secure: true,
             //httpOnly: true,
             sameSite: false, 
-            domain: 'https://pochi-api.vercel.app',
+            domain: 'https://pochi-api.onrender.com/',
         },
         store: store.create({
             mongoUrl: process.env.MONGO_URI
